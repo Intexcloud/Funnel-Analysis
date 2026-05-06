ba59c01fbdb2e2e47dafc52fa2ed4237af635972
Sandy Aprilyanto <128687467+Intexcloud@users.noreply.github.com>
2026-05-07 05:50:00 +0700

    Add files via upload

---
+++ sqlqueries/Average Order Value (AOV) by Category.sql
@@ -0,0 +1,15 @@
+SELECT
+    c.category_name,
+    ROUND(AVG(order_total), 2) AS aov
+FROM (
+    SELECT
+        oi.order_id,
+        p.category_id,
+        SUM(oi.quantity * oi.unit_price) AS order_total
+    FROM order_items oi
+    JOIN products p ON oi.product_id = p.product_id
+    GROUP BY oi.order_id, p.category_id
+) sub
+JOIN categories c ON sub.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY aov DESC;
---
+++ sqlqueries/Revenue by Category.sql
@@ -0,0 +1,13 @@
+SELECT
+    c.category_name,
+    SUM(oi.quantity * oi.unit_price) AS total_revenue,
+    ROUND(
+        SUM(oi.quantity * oi.unit_price) * 100.0
+        / SUM(SUM(oi.quantity * oi.unit_price)) OVER(),
+        2
+    ) AS revenue_percentage
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id
+JOIN categories c ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY total_revenue DESC;
